5e66f71d9505aafa619373130dc298aae1d35702
José Joaquim da Silva Neto <zeneto2508@gmail.com>
2022-04-29 04:30:01 -0300

    Add files via upload

---
+++ Lab1-Introdução.py
@@ -0,0 +1,43 @@
+#Estudo em Python
+#Lab 1
+
+#Exercício 1 
+#Faça um Programa que mostre a mensagem "Primeiro Programa em Python" na tela.
+
+print("Primeiro Programa em Python")
+
+#Exercicio 2 
+#Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B e imprima o resultado na tela.
+
+A=int(input())
+B=int(input())
+C=A+B
+print(C)
+
+#Exercicio 3 
+#A fórmula para calcular a área de uma circunferência é: area = π * raio2 . Considerando que π = 3.14159, efetue o cálculo da área para o valor de raio obtido do usuário. Exiba o resultado.
+
+a = int(input("Digite o valor do raio: "))
+b = 3.14159
+area = (b*a**2)
+print(area)
+
+#Exercicio 4
+#Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D). Então, exiba o resultado.
+
+a = int(input(""))
+b = int(input(""))
+c = int(input(""))
+d = int(input(""))
+e = "resultado"
+Resultado = (a*b-c*d)
+print(e, Resultado)
+
+#Exercicio 5 
+#Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável prod. A seguir mostre o resultado na tela.
+
+a = int(input("Entre com o primeiro número: "))
+b = int(input("Entre com o segundo número: "))
+Prod = (a*b)
+e = "prod ="
+print(e, Prod)
